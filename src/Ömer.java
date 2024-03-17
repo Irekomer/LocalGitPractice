7e087d95cbe2084dfaadba3987585756815bfb4a
Irekomer <160185136+Irekomer@users.noreply.github.com>
2024-03-17 16:09:53 +0100

    Remote da Ömer dosyasi güncellendi

--- src/Ömer.java
+++ src/Ömer.java
@@ -1,5 +1,7 @@
 public class Ömer {
     public static void main(String[] args) {
         System.out.println("Ömer branch in da eklenen code !!");
+        // Yeni satirlar eklendi 
+        System.out.println("Ömer branch ina Githup Remote dan eklemeler yaptim... !!");
     }
 }
