18b02a0d615d03030cfd906dbf092a24f97c870d
Ömer Irek <irekoemer@gmail.com>
2024-03-17 16:22:38 +0100

    local1 eski dosyalar eklendi

--- src/Local1.java
+++ src/Local1.java
@@ -1,6 +1,6 @@
 public class Local1 {
     public static void main(String[] args) {
-        System.out.println("GitTest");
+        System.out.println("GitTest bu testi degistirdim");
         System.out.println("Yeni satir eklendi!");
         System.out.println("Yeni test");
         System.out.println("Pazar");
