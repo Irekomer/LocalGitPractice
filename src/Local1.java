d45a9b1289ff305ce587dcb3043b07f7c4fe568a
Ömer Irek <irekoemer@gmail.com>
2024-03-17 14:43:50 +0100

    Local1 ve Local2 dosyalari güncellendi

---
+++ src/Local1.java
@@ -0,0 +1,9 @@
+public class Local1 {
+    public static void main(String[] args) {
+        System.out.println("GitTest");
+        System.out.println("Yeni satir eklendi!");
+        System.out.println("Yeni test");
+        System.out.println("Pazar");
+        System.out.println("Ömer branch in da eklenen code !!");
+    }
+}
---
+++ src/Local2.java
@@ -0,0 +1,8 @@
+public class Local2 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+        System.out.println("test2");
+        System.out.println("Pazar");
+        System.out.println("Ömer branch in da eklenen code !!");
+    }
+}
